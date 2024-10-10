80810ae57ac7771c78dbb64b3416c34f025b1485
Sirius1145 <m19907868324@163.com>
2024-10-10 11:11:27 +0800

    Initial Commit

---
+++ src/test/java/sirius/ticketingmanagementsystem/ticketingmanagementsystem/TicketingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sirius.ticketingmanagementsystem.ticketingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
